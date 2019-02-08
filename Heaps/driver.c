b4ec9d1e881d7b54718162e09701bd77b394af67
cslite <34658024+cslite@users.noreply.github.com>
2019-02-09 02:41:54 +0530

    Create driver.c

---
+++ Heaps/driver.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "heap.h"
+
+#define MAX_SIZE 30
+
+int main(int argc, char *argv[]){
+	if(argc == 1){
+		printf("No input file!\n");
+		return -1;
+	}
+	FILE *fptr = fopen(argv[1],"r");
+	if(fptr == NULL){
+		printf("Cannot open file!\n");
+		return -1;
+	}
+	int arr[MAX_SIZE];
+	int i=0;
+	while(i<MAX_SIZE && fscanf(fptr,"%d",&arr[i]) == 1)
+		i++;
+	printArr(arr,i);
+	heapSort(arr,i);
+	printArr(arr,i);
+}
